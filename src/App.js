fd81d8b23c77c01334d4daa4a903154ca2cb69c5
kirin608 <andrewshapi@gmail.com>
2021-10-26 18:52:18 -0400

    Embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
- 
+ const title = 'react';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {title}</h1>
+      <label htmlFor="search">Search</label>
+      <input id="search" type="text"/>
     </div>
   );
 }
